df3b8a3b4dd87fd8dca1808f30f5ac35535c6b40
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-05-29 20:13:54 -0300

    Add caesar cipher algorithm

---
+++ algorithms/caesar-cipher.js
@@ -0,0 +1,46 @@
+/**
+ * Given a string, return a new string where each letter is shifted a given amount of positions in the alphabet.
+ *
+ * Complexity: O(n)
+ */
+function caesarCipher(string,shift) {
+  // Handle numbers that are outside the alphabet length (e.g. -15, 677)
+  const SHIFT = shift % 26;
+
+  const lowerCaseString = string.toLowerCase();
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let newString = '';
+  
+  for (let i = 0; i < lowerCaseString.length; i++) {
+    let currentLetter = lowerCaseString[i];
+    
+    // Handle space character (just add it to the new string)
+    if (currentLetter === ' ') {
+      newString += currentLetter;
+      continue;
+    }
+    
+    const currentIndex = alphabet.indexOf(currentLetter);
+    let newIndex = currentIndex + SHIFT;
+    
+    // Handle corner cases where new index is greater than the length or smaller than zero
+    if (newIndex > 25) {
+      newIndex = newIndex - 26;
+    }
+    
+    if (newIndex < 0) {
+      newIndex = 26 + newIndex;
+    }
+
+    // Handle given uppercased letters
+    if (string[i] === string[i].toUpperCase()) {
+      newString += alphabet[newIndex].toUpperCase();
+    } else {
+      newString += alphabet[newIndex];
+    }
+  };
+  
+  return newString;
+}
+
+console.log(caesarCipher('Zoo Keeper', 4));
