f2bce9e6027965625aad8872351d608515703bcd
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-05-31 15:14:57 -0300

    Add factorial algorithm

---
+++ algorithms/factorial.js
@@ -0,0 +1,14 @@
+/**
+ * Given a number, return the factorial value of it
+ *
+ * Complexity: O(n)
+ */
+function factorial(num) {
+  if (num <= 2) {
+    return num;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(5));
