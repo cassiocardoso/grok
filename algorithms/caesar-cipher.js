c719c4e0fc3faec250cfbcd07aaf25f87810dbdd
Cassio Cardoso <caugusto.cardoso@gmail.com>
2018-05-31 21:58:20 -0300

    Update caesar cipher file

--- algorithms/caesar-cipher.js
+++ algorithms/caesar-cipher.js
@@ -1,33 +1,34 @@
 /**
- * Given a string, return a new string where each letter is shifted a given amount of positions in the alphabet.
+ * Given a string, return a new string where each letter is shifted
+ * by a given amount of positions in the alphabet.
  *
  * Complexity: O(n)
  */
-function caesarCipher(string,shift) {
+function caesarCipher(string, shift) {
   // Handle numbers that are outside the alphabet length (e.g. -15, 677)
   const SHIFT = shift % 26;
 
   const lowerCaseString = string.toLowerCase();
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   let newString = '';
-  
+
   for (let i = 0; i < lowerCaseString.length; i++) {
     let currentLetter = lowerCaseString[i];
-    
+
     // Handle space character (just add it to the new string)
     if (currentLetter === ' ') {
       newString += currentLetter;
       continue;
     }
-    
+
     const currentIndex = alphabet.indexOf(currentLetter);
     let newIndex = currentIndex + SHIFT;
-    
+
     // Handle corner cases where new index is greater than the length or smaller than zero
     if (newIndex > 25) {
       newIndex = newIndex - 26;
     }
-    
+
     if (newIndex < 0) {
       newIndex = 26 + newIndex;
     }
@@ -39,8 +40,6 @@ function caesarCipher(string,shift) {
       newString += alphabet[newIndex];
     }
   };
-  
+
   return newString;
 }
-
-console.log(caesarCipher('Zoo Keeper', 4));
