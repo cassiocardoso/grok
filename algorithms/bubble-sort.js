9140880fc0ecb3ce1d897c0e9e52f8452c8208a8
Cassio Cardoso <caugusto.cardoso@gmail.com>
2020-04-23 15:42:33 +0200

    make bubbleSort more readable

--- algorithms/bubble-sort.js
+++ algorithms/bubble-sort.js
@@ -1,20 +1,28 @@
 /**
  * Given an array, return it sorted using the bubble sort algorithm
  *
- * Complexity: O()
+ * Complexity: O(n^2)
  */
 function bubbleSort(array) {
-  for (let i = array.length; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (array[j] > array[j + 1]) {
-        let temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
+  for (let lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+  	for (let i = 0; i < lastUnsortedIndex; i++) {
+  		if (array[i] > array[i + 1]) {
+  			swap(array, i, i + 1);
+			}
+		}
   }
-  
+
   return array;
+}
+
+function swap(array, i, j) {
+	if (i === j) {
+		return;
+	}
+
+	let temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
 }
 
 console.log(bubbleSort([7, 7, 8, 2, 13, 31, -5, 23, 6, 4, 23, 56, 1, 0]));
